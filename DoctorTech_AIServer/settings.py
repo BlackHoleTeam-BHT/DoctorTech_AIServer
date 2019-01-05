c729c5e37a525e661911ffe59165cb1d33efffe4
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-05 22:24:09 +0300

    (fun) add the database schema and connect the algorithm with database

--- DoctorTech_AIServer/settings.py
+++ DoctorTech_AIServer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Diabetes.apps.DiabetesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
