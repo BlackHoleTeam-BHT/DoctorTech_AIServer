656306fef82c3129c7cb8e5783857ef0b95a9773
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-05 17:37:05 +0300

    (feat) add the dataset and create the algorithm

--- Diabetes/views.py
+++ Diabetes/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-# Create your views here.
+
+
+#Note : this function use RandomForestClassifier algorithm to predict the diabetes result
 def diabetesPredict(req):
+    dataFrame=pd.read_csv('Diabetes/dataset/diabetes.csv')
+    columns = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+    labels = dataFrame['Outcome'].values
+    for i in range(8):
+        print(dataFrame.values[i])
+    print(len(dataFrame))
     return HttpResponse('<p>eeee</p>')
+     
 
