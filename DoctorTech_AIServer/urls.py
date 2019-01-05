04e7877f889ea9007c6fff5f58d0d0e9263206d3
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-06 02:01:29 +0300

    (feat) add  health App

--- DoctorTech_AIServer/urls.py
+++ DoctorTech_AIServer/urls.py
@@ -4,5 +4,9 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('diabetes/',include('Diabetes.urls')),
+<<<<<<< HEAD
     path('breast-cancer/', include('BreastCancer.urls')),
+=======
+    path('health/',include('Health.urls'))
+>>>>>>> (feat) add  health App
 ]
---
+++ Health/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def healthPredict(req):
+    
+    return HttpResponse('1')
+
