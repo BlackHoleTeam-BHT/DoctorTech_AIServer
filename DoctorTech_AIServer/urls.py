0fdda5de00bff1bafbcf522d4a1d8b0cb154b962
mhd-rawashdah <mhd.rawashdah@gmail.com>
2019-01-06 02:32:30 +0200

    (feat) add BreastCancer app to predicate breast cancer and implementation Random forest algorthim of machine learning

--- DoctorTech_AIServer/urls.py
+++ DoctorTech_AIServer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('diabetes/',include('Diabetes.urls'))
+    path('diabetes/',include('Diabetes.urls')),
+    path('breast-cancer/', include('BreastCancer.urls')),
 ]
