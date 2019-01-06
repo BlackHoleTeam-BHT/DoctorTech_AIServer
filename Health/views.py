980a947fdb000bf0c0fa4c61b0fac7ea8d0dfbe0
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-06 03:05:46 +0300

    (feat) add Distance-Calories algorithm

--- Health/views.py
+++ Health/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sklearn import linear_model
+import pandas as pd 
+
+
 
 def healthPredict(req):
-    
+    ###########Distance-Calories####################################
+    dataFrameDC=pd.read_csv('Health/dataset/distanceCalories.csv')
+    DC_target=dataFrameDC[['distance']]
+    DC_input=dataFrameDC[['calories']]
+    LinearDC=linear_model.LinearRegression()
+    LinearDC.fit(DC_input,DC_target)
+    AcurecyDC=LinearDC.score(DC_input, DC_target)
+    print(LinearDC.predict([[500]]),AcurecyDC)
+    ##########Minimum-Wight######################################### 
+    dataFrameMW=pd.read_csv('Health/dataset/heightweight.csv')
+    print(dataFrameMW)
+    MW_target=dataFrameMW[['minimum']]
+    MW_input=dataFrameMW[['height ']]
+    LinearMW=linear_model.LinearRegression()
+    LinearMW.fit(MW_input,MW_target)
+    AcurecyMW=LinearMW.score(MW_input, MW_target)
+    print(LinearMW.predict([[160]]),AcurecyMW)
+
+
     return HttpResponse('1')
 
