97c1925e048fb6ce138fb3c5b8d85f4e64f7916f
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-01-09 14:11:18 +0300

    (fun) connect the HeartAttck app with database

--- HeartAttack/admin.py
+++ HeartAttack/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
+from.models import HeartAttack
 
-# Register your models here.
-# admin.site.register("Diabetes")
+admin.site.register(HeartAttack)
--- HeartAttack/models.py
+++ HeartAttack/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class HeartAttack(models.Model):
+    age=models.DecimalField(max_digits=10, decimal_places=4)
+    sex=models.DecimalField(max_digits=10, decimal_places=4)
+    cp=models.DecimalField(max_digits=10, decimal_places=4)
+    trestbps=models.DecimalField(max_digits=10, decimal_places=4)
+    chol=models.DecimalField(max_digits=10, decimal_places=4)
+    fbs=models.DecimalField(max_digits=10, decimal_places=4)
+    restecg=models.DecimalField(max_digits=10, decimal_places=4)
+    thalach=models.DecimalField(max_digits=10, decimal_places=4)
+    exang=models.DecimalField(max_digits=10, decimal_places=4)
+    oldpeak=models.DecimalField(max_digits=10, decimal_places=4)
+    slop=models.DecimalField(max_digits=10, decimal_places=4)
+    ca=models.DecimalField(max_digits=10, decimal_places=4)
+    thal=models.DecimalField(max_digits=10, decimal_places=4)
+    pred_attribute=models.DecimalField(max_digits=10, decimal_places=4)
